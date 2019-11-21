de3d98c1a6e7850ff0a3a851db4af9a04992aec8
Chris McClure <chrismcclure2010@gmail.com>
2019-11-20 22:19:06 -0900

    working basic structure of webserver

--- RSNAHL/rsnahl/rsnahl/league/models.py
+++ RSNAHL/rsnahl/rsnahl/league/models.py
@@ -3,28 +3,44 @@ from django.urls import reverse
 
 class Person(models.Model):
     first_name = models.CharField(max_length=64, default="")
-    middle_initial = models.CharField(max_length=1, default='')
+    middle_initial = models.CharField(max_length=1, default="")
     last_name = models.CharField(max_length=64, default="")
     ssn = models.IntegerField(default=0)
-    dob = models.DateField()
+    dob = models.CharField(max_length=20, default="")
     pob = models.CharField(max_length=200, default="")
 
     def __str__(self):
-        self.first_name + " " + self.middle_initial + " " + self.last_name
+        return self.first_name + " " + self.middle_initial + " " + self.last_name
 
 class Player(models.Model):
-    first_name = Person.first_name
-    middle_initial = Person.middle_initial
-    last_name = Person.last_name
-    ssn = Person.ssn
-    dob = Person.dob
-    pob = Person.pob
+    player = models.ForeignKey(Person, on_delete=models.CASCADE)
     number = models.IntegerField(default=0)
     weight = models.IntegerField(default=0)
     height = models.CharField(max_length=10, default="")
-    position = models.CharField(max_length=20, default="") #should consist of "left" and "right"
-    stick_orientation = models.CharField(max_length=5, default="")
+    POSITION_CHOICES = [
+        ('Forward', (
+                ('C', 'Center'),
+                ('RW', 'Right Wing'),
+                ('LW', 'Left Wing'),
+            )
+        ),
+        ('Defense', (
+                ('RD', 'Right Defense'),
+                ('LD', 'Left Defense'),
+            )
+        ),
+        ('Goalie', 'Goalie'),
+    ]
+    # position = models.CharField(max_length=20, default="") #should consist of "left" and "right"
+    position = models.CharField(max_length=20, choices=POSITION_CHOICES, default='')
 
+    ORIENTATION_CHOICES = [
+        ('Right', 'Right'), ('Left', 'Left'),
+    ]
+    stick_orientation = models.CharField(max_length=5, choices=ORIENTATION_CHOICES, default='')
+
+    def __str__(self):
+        return ("%s %s" % (self.player.first_name, self.player.last_name))
 
 
 class Team(models.Model):
@@ -36,25 +52,21 @@ class Team(models.Model):
         return ("%s %s" % (self.city, self.name))
 
 class Coach(models.Model):
-    first_name = Person.first_name
-    middle_initial = Person.middle_initial
-    last_name = Person.last_name
-    ssn = Person.ssn
-    dob = Person.dob
-    pob = Person.pob
+    coach = models.ForeignKey(Person, null=True, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, null =True, on_delete=models.CASCADE)
+    head = models.BooleanField(default=False)
+
+    def __str__(self):
+        return ("%s %s" % (self.coach.first_name, self.coach.last_name))
 
 class Manager(models.Model):
-    first_name = Person.first_name
-    middle_initial = Person.middle_initial
-    last_name = Person.last_name
-    ssn = Person.ssn
-    dob = Person.dob
-    pob = Person.pob
+    manager = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ("%s %s" % (self.manager.first_name, self.manager.last_name))
 
 class Referee(models.Model):
-    first_name = Person.first_name
-    middle_initial = Person.middle_initial
-    last_name = Person.last_name
-    ssn = Person.ssn
-    dob = Person.dob
-    pob = Person.pob
+    referee = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ("%s %s" % (self.referee.first_name, self.referee.last_name))
--- RSNAHL/rsnahl/rsnahl/rsnahl/settings.py
+++ RSNAHL/rsnahl/rsnahl/rsnahl/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'rsnahl',
-#         'USER': 'chrismcclure',
-#         'PASSWORD': 'password',
+#         'USER': 'root',
+#         'PASSWORD': '',
 #         'HOST': 'localhost',
 #         'PORT': '3306',
 #     }
